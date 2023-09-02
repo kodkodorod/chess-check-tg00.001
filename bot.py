0588b5fe4a7b9d714ed6275cd62f0f8b91972640
kodkodorod <114239871+kodkodorod@users.noreply.github.com>
2023-09-02 23:21:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,46 @@
+import chess
+import chess.svg
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler
 
-   import logging
-   from telegram.ext import Updater, CommandHandler
+def start(update, context):
+    update.message.reply_text('Halo! Untuk memulai permainan, ketik "/main".')
 
-   # Inisialisasi bot dengan token dari BotFather
-   updater = Updater(token='6592288337:AAGw0ni9sboukpm2-liE-j1bDpGHl5G98zo', use_context=True)
-   dispatcher = updater.dispatcher
+def play_chess(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    board = chess.Board()
+    moves = query.split()
+    for move in moves:
+        try:
+            board.push_san(move)
+        except ValueError:
+            break
+    if board.is_game_over():
+        results.append(
+            InlineQueryResultArticle(
+                id='1',
+                title='Permainan Selesai',
+                input_message_content=InputTextMessageContent('Permainan Selesai.'))
+        )
+    else:
+        results.append(
+            InlineQueryResultArticle(
+                id='1',
+                title='Permainan Berlanjut',
+                input_message_content=InputTextMessageContent(board.fen()))
+        )
+    update.inline_query.answer(results)
 
-   # Fungsi untuk menangani perintah /start
-   def start(update, context):
-       context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a chess bot.")
+def main():
+    updater = Updater('6592288337:AAGw0ni9sboukpm2-liE-j1bDpGHl5G98zo', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(InlineQueryHandler(play_chess))
+    dp.add_handler(CommandHandler('start', start))
+    updater.start_polling()
+    updater.idle()
 
-   # Menambahkan handler perintah /start ke bot
-   start_handler = CommandHandler('start', start)
-   dispatcher.add_handler(start_handler)
-
-   # Menjalankan bot
-   updater.start_polling()
-   
+if name == 'main':
+    main()
